cee5be542ae02ff0ea32326b9f9e9f4bec05e92d
biswajit.paul <biswajit.paul@infraplan.co.in>
2023-06-24 18:22:02 +0530

    backend Url added

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://weary-yak-cummerbund.cyclic.app/" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("Profile")) {
